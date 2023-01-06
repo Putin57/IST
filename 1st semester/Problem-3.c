e4aad52754d34594c977772f995228f2e9cf826d
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-01-06 16:38:39 +0600

    Add files via upload

---
+++ 1st semester/Problem-3.c
@@ -0,0 +1,7 @@
+// Question : About yourself
+
+#include <stdio.h>
+ int main(){
+    printf("\"Welcome to IST\"\nCSE Department\nI am Mahidul Islam.\nRoll: 22057\nSession: 2021-22");
+    return 0;
+ }
