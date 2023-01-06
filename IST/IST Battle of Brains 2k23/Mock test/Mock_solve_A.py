9e4cf7a9c7587cf3f5cdad8ca281d1d99b5c84ec
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-01-06 16:37:43 +0600

    Delete IST directory

--- IST/IST Battle of Brains 2k23/Mock test/Mock_solve_A.py
+++
@@ -1,5 +0,0 @@
-t = int(input())
-if t>20:
-    print("Hot")
-else:
-    print("Cold")
--- IST/IST Battle of Brains 2k23/contest/solve_B.py
+++
@@ -1,8 +0,0 @@
-a,x,b = input().split()
-a=int(a)
-b=int(b)
-if x == "+":
-    print(a+b)
-else:
-    print(a-b)
-    
