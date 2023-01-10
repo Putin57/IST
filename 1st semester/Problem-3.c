91f513b56d996203a366804f28bdf8559ed58a5b
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-01-11 01:05:26 +0600

    Delete Problem-3.c

--- 1st semester/Problem-3.c
+++
@@ -1,7 +0,0 @@
-// Question : About yourself
-
-#include <stdio.h>
- int main(){
-    printf("\"Welcome to IST\"\nCSE Department\nI am Mahidul Islam.\nRoll: 22057\nSession: 2021-22");
-    return 0;
- }
