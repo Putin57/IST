b9ebdf41852e8c0945b47005995289d45e0be2d2
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-03-07 15:37:15 +0600

    Add files via upload
    Temperature converter for Celsius to Fahrenheit and vice versa...

---
+++ 1st semester/Temperature Converter.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(){
+    float c,f,celcius,fahrenheit;
+    int n;
+    char x;
+    printf("### Temerature Converter ###\n");
+    printf("1.Convert Celcius to Fahrenheit.\n");
+    printf("2.Convert Fahrenheit to Celcius.\n");
+    while(1){
+    printf("Enter type a number (1/2): ");
+    scanf("%d",&n);
+    if (n==1||n==2){
+        if (n==1){
+            printf("\nEnter Celcious value: ");
+            scanf("%f",&c);
+            fahrenheit=((9*c)/5)+32;
+            printf("Fahrenheit: %.2f",fahrenheit);
+            printf("\nDo you want to Continue?(Y/N): ");
+            scanf(" %c",&x);
+            if (x=='y'||x=='Y'){continue;}
+            else{printf("Thank You..");
+            break;}
+            }
+        else if(n==2){
+            printf("\nEnter Fahrenheit value: ");
+            scanf("%f",&f);
+            celcius=(5*(f-32))/9;
+            printf("Celcious: %.2f",celcius);
+            printf("\nDo you want to Continue?(Y/N): ");
+            scanf(" %c",&x);
+            if (x=='y'||x=='Y'){continue;}
+            else{printf("\nThank You..");
+            break;}
+        }}
+    else{
+        printf("\nEnter valid Number.");
+        printf("\nDo you want to Continue?(Y/N): ");
+            scanf(" %c",&x);
+            if (x=='y'||x=='Y'){continue;}
+            else{printf("Thank You..");
+            break;}
+        }
+    }
+    return 0;
+}
