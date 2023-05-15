acf1da6dd830641806c233842c55be97239fb495
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-05-15 22:34:49 +0600

    Add files via upload

---
+++ 1st semester/Lab exam/section-2/11.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct student{
+    int id;
+    char name[50];
+    int mark;
+};
+
+int main(){
+    struct student s[5];
+    int i;
+
+    printf("\nThe records only for 5 student\n\n");
+
+    for(i=0;i<5;i++){
+    printf("\nRoll no : ");
+    scanf("%d",&s[i].id);
+    printf("Enter Name : ");
+    scanf("%s",s[i].name);
+    printf("Enter Marks : ");
+    scanf("%d",&s[i].mark);
+    }
+
+    printf("\nData of 5 students");
+
+    for(i=0;i<5;i++){
+        printf("\nID no : %d\t Name : %s\t Marks : %d",s[i].id,s[i].name,s[i].mark);
+    }
+
+    printf("\n\n");
+    exit(0);
+    getch();
+    return 0;
+}
