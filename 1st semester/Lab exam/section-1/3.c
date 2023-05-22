5459de7ce334641ef170c06478cdd17b9d69b903
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-05-22 19:36:56 +0600

    Update 3.c

--- 1st semester/Lab exam/section-1/3.c
+++ 1st semester/Lab exam/section-1/3.c
@@ -3,7 +3,7 @@ int main(){
     float length,wide;
     printf("\nEnter the Length of rectangle: ");
     scanf("%f",&length);
-    printf("\nEnter the wide of wide: ");
+    printf("\nEnter the wide of rectangle: ");
     scanf("%f",&wide);
     printf("\nThe area of rectangle: %.2f\n",length*wide);
     printf("\n");
