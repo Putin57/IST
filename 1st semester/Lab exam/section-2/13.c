0b8aa2132c1817ea3bf96fb762f1aa0456b5628b
Mahidul Islam <75845610+M4H1D@users.noreply.github.com>
2023-05-24 22:30:33 +0600

    Update 13.c

--- 1st semester/Lab exam/section-2/13.c
+++ 1st semester/Lab exam/section-2/13.c
@@ -10,3 +10,27 @@ int main(){
     fclose(f2);
     return 0;
 }
+
+
+
+/*
+1st we will create "INPUT.txt" file.
+But it must be placed where this C file is created.
+Then open the file and give some input there.
+Then save the file. Then write the code in the CodeBlocks and run it.
+It will be seen that another file called "copy.txt" will be seen.
+If you open that "copy.txt" file, you will see only the 1st letter of what we wrote in the "INPUT.txt" file.
+There is nothing to panic. Only 1 character will appear in this task....
+
+........Happpy Coding.......
+
+
+created by  
+___  ___      _     _     _ 
+|  \/  |     | |   (_)   | |
+| .  . | __ _| |__  _  __| |
+| |\/| |/ _` | '_ \| |/ _` |
+| |  | | (_| | | | | | (_| |
+\_|  |_/\__,_|_| |_|_|\__,_|
+
+*/
