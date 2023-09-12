37ea0a99404e170672b50b3a91e73829737d5b6f
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2023-09-13 02:41:32 +0600

    Update 9_(fibonacci).c

--- 1st semester/Lab exam/section-1/9_(fibonacci).c
+++ 1st semester/Lab exam/section-1/9_(fibonacci).c
@@ -6,13 +6,13 @@ int main(){
     int x=0,y=1;
     printf("The first %d fibonacci series is : ",n);  
     if(n==1){
-        printf("%d\n",x);  # if user input only 1
+        printf("%d\n",x);  // if user input only 1
     }else if(n==2){
-        printf("%d ",x);   # if user input 2
+        printf("%d ",x);   // if user input 2
         printf("%d ",y);
         printf("\n");
     }else{
-        printf("%d ",x);   # if user input 3 and so on
+        printf("%d ",x);   // if user input 3 and so on
         printf("%d ",y); 
         for(int i=2;i<n;i++){
             int z=x+y;
