045f2d5a733811fe8e12635a69849f12ede075ec
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2023-09-13 06:51:49 +0600

    Update 12_(input_file).c

--- 1st semester/Lab exam/section-2/12_(input_file).c
+++ 1st semester/Lab exam/section-2/12_(input_file).c
@@ -1,18 +1,15 @@
 #include<stdio.h>
 int main(){
-    FILE *fp;
-    char ch;
-    fp=fopen("INPUT.txt","w");
-    ch=getchar();
-    while(ch!=EOF){
-        fputc(ch,fp);
-        ch=getchar();
+    FILE *file;
+    char text[30];
+    file=fopen("INPUT.txt","w");
+    gets(text);
+    fputs(text,file);
+    fclose(file);
+    file=fopen("INPUT.txt","r");
+    while(fgets(text,sizeof(text),file)!=NULL){
+        printf("%s",text);
     }
-    fclose(fp);
+    fclose(file);
     return 0;
 }
-
-
-/*
-After running the code here, typing something in the input and pressing ctrl+z will stop it and you can see the output in the "INPUT.txt" file...
-*/
