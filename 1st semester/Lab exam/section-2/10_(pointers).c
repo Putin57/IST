193bc529c5851d6d2ed7560540fcec558b369b7d
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2023-09-13 06:03:08 +0600

    Update 10_(pointers).c

--- 1st semester/Lab exam/section-2/10_(pointers).c
+++ 1st semester/Lab exam/section-2/10_(pointers).c
@@ -1,23 +1,21 @@
 #include<stdio.h>
-#include<conio.h>
 #define MAX 30
-
 void main(){
-int size,i,arr[MAX];
-int *ptr;
-ptr=&arr[0];
-printf("Enter the size of array: ");
-scanf("%d",&size);
-printf("Enter %d integers into array: ",size);
-for(i=0;i<size;i++){
-    scanf("%d",ptr);
-    ptr++;
-}
-ptr=&arr[size-1];
-printf("Elements of array in reverse order are: ");
-for(i=size-1;i>=0;i--){
-    printf("%d ",*ptr);
-    ptr--;
-}
-getch();
+    int size,arr[MAX];
+    int *ptr;
+    ptr=&arr[0];
+    printf("Enter the size of array: ");
+    scanf("%d",&size);
+    printf("Enter %d integer elements: ",size);
+    for(int i=0;i<size;i++){
+        scanf("%d",ptr);
+        ptr++;
+    }
+    ptr=&arr[size-1];
+    printf("Elements of array in reverse order are: ");
+    for(int i=size-1;i>=0;i--){
+        printf("%d ",*ptr);
+        ptr--;
+    }
+    printf("\n");
 }
