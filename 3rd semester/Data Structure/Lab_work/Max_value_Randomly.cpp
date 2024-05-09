ec51ffee6365366219f16cb2da6b06a8b6bbba72
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-05-09 22:44:00 +0600

    Create Max_value_Randomly.cpp

---
+++ 3rd semester/Data Structure/Lab_work/Max_value_Randomly.cpp
@@ -0,0 +1,51 @@
+// find the max value of an array from computer (actually genarate random value from computer) using while loop..
+/*
+------------------------------------------------------------------------
+|Algorithm :                                                            |
+|step 1. K := 1, LOC := 1 and MAX := DATA[1]                            |
+|step 2. Repeat steps 3 and 4 while K <= N:                             |
+|step 3. If MAX < DATA[K], then :                                       |
+|                set LOC := K and MAX := DATA[K]:                       |
+|step 4. set K := K+                                                    |
+|step 5. Write LOC, MAX                                                 |
+|step 6. Exit                                                           |
+------------------------------------------------------------------------
+here, 
+DATA = array
+LOC = Location / Index
+
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout << "Enter the size of array : ";
+    int n;
+    cin >> n;
+    int data[n];
+    for(int i = 0; i < n; i++){
+        data[i] = rand();
+    }
+    cout << '\n';
+    cout << "All the elements of array which is generate by randomly in comuter, they are : ";
+    cout << '\n';
+    for(int i = 0; i < n; i++){
+        cout << data[i] << ' ';
+    }
+    cout << '\n';
+
+    int k = 0;
+    int mx = 0;
+    int loc = 0;
+    while(k < n){
+        if(mx < data[k]){
+            mx = data[k];
+            loc = k + 1;
+        }
+        k++;
+    }
+    cout << "Maximum value is : " << mx << endl;
+    cout << "Maximum value index is : " << loc << endl;
+    
+    return 0;
+}
