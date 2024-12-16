37f4f84fd863d2b8a0662805bebab798969dc9ad
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-12-17 03:25:45 +0600

    Update 7.cpp

--- 3rd semester/Lab_Report/Data Structure Lab report/7.cpp
+++ 3rd semester/Lab_Report/Data Structure Lab report/7.cpp
@@ -1,51 +1,55 @@
-// 7. Write a program to replace a substring by another substring into text
-
+//7. write a program to sort an array using quick sort algorithm
 /*
-----------------------------------------
-|Algorithm:                            |
-|step-1: set K:= Index(T,P)            |
-|step-2: Repeat while K!=0             |
-|            (a)set T:= Replace(T,P,Q) | 
-|            (b)set K:= Index(T,P)     | 
-|step-3: write: T                      | 
-|step-4: Exit                          | 
------------------------------------------
+-----------------------------------------------------------------
+|Algorithm:                                                     |
+|step-1: Top:=NULL                                              |
+|step-2: If N>1, then:Top:=Top+1, Lower[1]:=1, Upper[1]:=N      |
+|step-3: Repeat steps 4 to 7 while Top != NULL                  |
+|step-4:     set Beg:=Lower[Top], End:=Upper[Top], Top:=Top-1   |
+|step-5:     call Quick(A,N,Beg,End, Loc)                       |
+|step-6:     If Beg<Loc-1, then:                                |
+|                Top:=Top+1, Lower[Top]:=Beg, Upper[Top]=Loc-1  |
+|step-7:     If Loc+1 < End, then:                              |
+|                Top:Top+1, Lower[Top]:=Loc+1, Upper[Top]:=End  |
+|step-8: Exit                                                   |    
+-----------------------------------------------------------------
 */
-
 #include <bits/stdc++.h>
-using namespace std;
-int main() {
-    string text, old_sub, new_sub, result;
-    cout << "\nEnter the text: ";
-    getline(cin, text);
-    int text_len = text.size();
-    cout << "Enter the substring to replace: ";
-    getline(cin, old_sub);
-    int old_sub_len = old_sub.size();
-    cout << "Enter the new substring: ";
-    getline(cin, new_sub);
-    for(int i = 0; i < text_len; i++){
-        bool flag = true;
-        for(int j = 0; j < old_sub_len; j++){
-            if(i + j >= text_len or text[i+j] != old_sub[j]){
-                flag = false;
-                break;
-            }
-        }
-        if(flag){
-            result += new_sub;
-            i += old_sub_len - 1;
-        }
-        else result += text[i];
+using namespace std; 
+
+vector<int>quick_sort(const vector <int> &arr){
+    if(arr.size() <= 1) return arr;
+    int pivot = arr[0];
+    vector<int>smaller, greater;
+    for(int i = 1; i < arr.size(); i++){
+        if(arr[i] <= pivot) smaller.push_back(arr[i]);
+        else greater.push_back(arr[i]);
     }
-    cout << "Final text: " << result << '\n' << '\n';
+    vector<int>smaller_sorted = quick_sort(smaller), greater_sorted = quick_sort(greater);
+    smaller_sorted.push_back(pivot);
+    smaller_sorted.insert(smaller_sorted.end(), greater_sorted.begin(), greater_sorted.end());
+    return smaller_sorted;
+}
+
+int main(){
+    cout << "\nEnter the size of array: ";
+    int n; cin >> n;
+    cout << "Enter the element of array: ";
+    vector<int>arr(n),final_sort;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    cout << "Quick Sort: ";
+    final_sort = quick_sort(arr);
+    for(int i: final_sort) cout << i << ' ';
+    cout << '\n' << '\n';
     return 0;
 }
 
 
 /*
--------------------------------------------------------------------------------
-time complexity: O(text_lenngth × old_substring_length)
-space complexity: O(text_length + old_substring_length + new_substring_length)
--------------------------------------------------------------------------------
+-------------------------------------------
+time complexity: 
+  i) best case / average case: O(n log(n))
+  ii) worst case: O(n^2)
+space complexity: O(n)
+-------------------------------------------
 */
