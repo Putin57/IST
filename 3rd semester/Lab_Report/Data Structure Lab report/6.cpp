8e2749fb8417ff92592611e1fdfa99928f6debd3
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-12-17 03:24:58 +0600

    Update 6.cpp

--- 3rd semester/Lab_Report/Data Structure Lab report/6.cpp
+++ 3rd semester/Lab_Report/Data Structure Lab report/6.cpp
@@ -1,47 +1,49 @@
-// 6. Write a program to implement Tower of Hanoi problem by using recursive function
-
+// 6. Write a program to sort an array using bubble sort
 /*
------------------------------------------
-|Algorithm:                             |
-|Tower(N, Beg, Aux, End)                |
-|step-1: If N=1, then:                  |
-|            (a)write: Beg --> End      |
-|            (b)return                  |
-|step-2: call Tower(N-1, Beg, End,Aux)  |
-|step-3: write Tower(N-1, Aux, Beg, End)|
-|step-4: call Tower(N-1, Aux, Beg, End) |
-|step-5: Return                         |
------------------------------------------
+--------------------------------------------------------
+|Algorithm:                                            |
+|step-1: Repeat steps 2 and 3 for K=1 to N-1           | 
+|step-2: set ptr:=1                                    | 
+|step-3: Repeat while ptr<=N-K:                        | 
+|            (a)If Data[ptr]>Data[ptr+1],Then:         | 
+|                Interchange Data[ptr] and Data[ptr+1] | 
+|            (b)set ptr:=ptr+1                         | 
+|step-4: Exit                                          | 
+--------------------------------------------------------
 */
 
 #include <bits/stdc++.h>
-using namespace std;
- 
+using namespace std; 
 
-void tower(int n, string begin, string aux, string end){
-    if(n == 1){
-        cout << begin <<" --> " << end << "\n";
-        return;
-    }
-    tower(n-1, begin, end, aux);
-    cout << begin << " --> " << end << "\n";
-    tower(n-1, aux, begin, end);
-    return;
- 
-}
 int main(){
-    cout << "\nEnter the number : ";
+    cout << "\nEnter the size of array: ";
     int n; cin >> n;
-    string a = "A", b = "B", c= "C";
-    tower(n, a, b, c);
+    int arr[n + 1];
+    cout << "Enter the element of array: ";
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n - i - 1; j++){
+            if(arr[j] > arr[j + 1]){
+                int tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+    cout << "After the sorting of the array is : ";
+    for(int i = 1; i <= n; i++){
+        cout << arr[i] << ' ';
+    }
+    cout << '\n' << '\n';
     return 0;
 }
 
 
-
 /*
---------------------------
-time complexity: O(2^n)
+-------------------------
+time complexity: O(n^2)
 space complexity: O(n)
---------------------------
+-------------------------
 */
