942e7003061a9bb55a05cb50658c9ec10dd108ca
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-12-17 03:24:03 +0600

    Update 5.cpp

--- 3rd semester/Lab_Report/Data Structure Lab report/5.cpp
+++ 3rd semester/Lab_Report/Data Structure Lab report/5.cpp
@@ -1,35 +1,51 @@
-//5. Write program to calculate factorial of a number using recursion
+// 5. Write a program to replace a substring by another substring into text
+
 /*
------------------------------------------
-|Algorithm:                             |
-|step-1: If N=0, set fact:=1 and return |
-|step-2: call factorial(fact, N-1)      | 
-|step-3: set fact:=N*fact               | 
-|step-4: return                         | 
------------------------------------------
+----------------------------------------
+|Algorithm:                            |
+|step-1: set K:= Index(T,P)            |
+|step-2: Repeat while K!=0             |
+|            (a)set T:= Replace(T,P,Q) | 
+|            (b)set K:= Index(T,P)     | 
+|step-3: write: T                      | 
+|step-4: Exit                          | 
+----------------------------------------
 */
+
 #include <bits/stdc++.h>
 using namespace std;
-
-int factorial(int n){
-    if(n == 0){
-        return 1;
+int main() {
+    string text, old_sub, new_sub, result;
+    cout << "\nEnter the text: ";
+    getline(cin, text);
+    int text_len = text.size();
+    cout << "Enter the substring to replace: ";
+    getline(cin, old_sub);
+    int old_sub_len = old_sub.size();
+    cout << "Enter the new substring: ";
+    getline(cin, new_sub);
+    for(int i = 0; i < text_len; i++){
+        bool flag = true;
+        for(int j = 0; j < old_sub_len; j++){
+            if(i + j >= text_len or text[i+j] != old_sub[j]){
+                flag = false;
+                break;
+            }
+        }
+        if(flag){
+            result += new_sub;
+            i += old_sub_len - 1;
+        }
+        else result += text[i];
     }
-    return n * factorial(n - 1);
-}
-
-int main(){
-    int n;
-    cout << "Enter the positive number: ";
-    cin >> n;
-    cout << "The Factorial of " << n << " is : ";
-    cout << factorial(n) << '\n';
+    cout << "Final text: " << result << '\n' << '\n';
     return 0;
 }
 
+
 /*
-----------------------
-time complexity: O(n)
-space complexity: O(n)
-----------------------
+-------------------------------------------------------------------------------
+time complexity: O(text_lenngth × old_substring_length)
+space complexity: O(text_length + old_substring_length + new_substring_length)
+-------------------------------------------------------------------------------
 */
