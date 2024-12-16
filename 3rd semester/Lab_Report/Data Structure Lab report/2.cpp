355eec1bb5f074f1bf2ef173d0bb0814556d20e4
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-12-17 03:21:49 +0600

    Update 2.cpp

--- 3rd semester/Lab_Report/Data Structure Lab report/2.cpp
+++ 3rd semester/Lab_Report/Data Structure Lab report/2.cpp
@@ -1,18 +1,21 @@
-// 2. Write a program to search an element from an array using Linear Search algorithm.
+// 2. Write a program to search an element from an array using binary search algorithm.
 /*
------------------------------------------------------
-|Algorithm:                                          |
-|step-1: set K:=1 and LOC :=0                        |
-|step-2: Repeat steps 3 and 4 while LOC=0 and k<=N   |
-|step-3:     If ITEM=Data[k],then:                   |
-|                set LOC:=K                          |
-|step-4:     set K:=k+1                              |
-|step-5: If LOC=0, then:                             |
-|            write:Item is not in array Data         |
-|        else:                                       |
-|            write: LOC is location of Item          |
-|step-6: Exit                                        |
------------------------------------------------------
+--------------------------------------------------------------------
+|Algorithm:                                                        |
+|step-1: set Beg:=LB, End:=UB and                                  |
+|        mid=Int((Beg + End))                                      |
+|step-2: Repeat steps 3 and 4 while Beg <= End and data[mid]!=Item |
+|step-3: If Item<Data[mid],then:                                   |
+|            set End:=mid-1                                        |
+|        else:                                                     |
+|            set Beg:=mid+1                                        |
+|step-4: set Mid:=Int((Beg+End)/2)                                 |
+|step-5: If Data[mid]=Item, then:                                  |
+|          set LOC:=mid                                            |
+|        Else:                                                     |
+|            set LOC:=NULL                                         |
+|step-6: Exit                                                      |
+_-------------------------------------------------------------------
 */
 
 #include <bits/stdc++.h>
@@ -22,26 +25,37 @@ int main(){
     int n, search_item;
     cout << "Enter the size of array: ";
     cin >> n;
-    int arr[n + 1];
-    cout << "Enter the elements of the Array: ";
+    int arr[n+1];
+    cout << "Enter the sorted element of the array: ";
     for(int i = 1; i <= n; i++){
         cin >> arr[i];
     }
-    cout <<"Search element: ";
+    cout << "Enter the search item: ";
     cin >> search_item;
-    for(int i = 1; i <= n; i++){
-        if(arr[i] == search_item){
-            cout << "Search Element found in index " << i << '\n';
+    int begin = 1, endd = n + 1;
+    while (begin <= endd){
+        int mid = (begin + endd)/2;
+        if(arr[mid] == search_item){
+            cout << "Search element found in index " << mid << '\n';
             return 0;
         }
-    }
-    cout << "Sorry!! Element not found..\n";
+        else if(arr[mid] < search_item){
+            begin = mid + 1;
+        }
+        else{
+            endd = mid - 1;
+        }
+    } 
+    cout << "Sorry!!.. The search item not found in the array..\n";
     return 0;
 }
 
+
+
+
 /*
-------------------
-time complexity: O(n)
+----------------------------
+time complexity: O(log(n))
 space complexity: O(n)
-------------------
+---------------------------
 */
