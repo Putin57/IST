cc9763c6921a723649516a6abe5be0c32b3972a4
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-12-17 03:19:47 +0600

    Update 1.cpp

--- 3rd semester/Lab_Report/Data Structure Lab report/1.cpp
+++ 3rd semester/Lab_Report/Data Structure Lab report/1.cpp
@@ -1,50 +1,49 @@
-//1.Find the Smallest and Largest element of the Array
+// 1. Write a program to search an element from an array using Linear Search algorithm.
 /*
------------------------------------------------------------
-Algorithm:						    
-step-1: Set K:=1,LOC:=1,Max:=Data[1] and Min:= Data[1]     
-setp-2: Repeat steps 3 to 5 while k<=N: 		    
-step-3: 	If Max < Data[K], then: 		    
-				set LOC:=K and Max:=Data[K] 
-step-4: 	If Min>Data[k], then: 			    
-				set LOC:=k and Min:=Data[K] 
-step-5: 	set K:=K+1				    
-step-6: Write: LOC, Max, Min				    
-step-7: Exit.						    	
------------------------------------------------------------
+|----------------------------------------------------|
+|Algorithm:                                          |
+|step-1: set K:=1 and LOC :=0                        |
+|step-2: Repeat steps 3 and 4 while LOC=0 and k<=N   |
+|step-3:     If ITEM=Data[k],then:                   |
+|                set LOC:=K                          |
+|step-4:     set K:=k+1                              |
+|step-5: If LOC=0, then:                             |
+|            write:Item is not in array Data         |
+|        else:                                       |
+|            write: LOC is location of Item          |
+|step-6: Exit                                        |
+|----------------------------------------------------|
 */
 
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
 
 int main(){
-	int n, mx = INT_MIN, mn = INT_MAX;
-	cout << "Enter the size of Array : ";
-	cin >> n;
-	int arr[n];
-	cout << "\nEnter the element of Array : ";
-	for(int i = 0; i < n; i++){
-		cin >> arr[i];
-	}
-	for(int i = 0; i < n; i++){
-		if(mx < arr[i]){
-			mx = arr[i];
-		}
-	}
-	for(int i = 0; i < n; i++){
-		if(mn > arr[i]){
-			mn = arr[i];
-		}
-	}
-	cout << "\nThe Smallest element of the array is: " << mn << '\n';
-	cout << "The Largest element of the array is: " << mx << '\n';
-	cout << '\n';
-	return 0;
+    int n, search_item;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n + 1];
+    cout << "Enter the elements of the Array: ";
+    for(int i = 1; i <= n; i++){
+        cin >> arr[i];
+    }
+    cout <<"Search element: ";
+    cin >> search_item;
+    for(int i = 1; i <= n; i++){
+        if(arr[i] == search_item){
+            cout << "Search Element found in index " << i << '\n';
+            return 0;
+        }
+    }
+    cout << "Sorry!! Element not found..\n";
+    return 0;
 }
 
 
-/*-------------------
+
+/*
+------------------------
 time complexity: O(n)
 space complexity: O(n)
--------------------*/
-
+------------------------
+*/
