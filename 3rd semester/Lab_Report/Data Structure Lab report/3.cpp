02ebf31ff93ee86e43e2fd04e260dd8b3cd164ea
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-12-17 03:22:45 +0600

    Update 3.cpp

--- 3rd semester/Lab_Report/Data Structure Lab report/3.cpp
+++ 3rd semester/Lab_Report/Data Structure Lab report/3.cpp
@@ -1,58 +1,36 @@
-// 3. Write a program to search an element from an array using binary search algorithm.
+//3. Write program to calculate factorial of a number using recursion
 /*
---------------------------------------------------------------------
-|Algorithm:                                                        |
-|step-1: set Beg:=LB, End:=UB and                                  |
-|        mid=Int((Beg + End)/2)                                    |
-|step-2: Repeat steps 3 and 4 while Beg <= End and data[mid]!=Item |
-|step-3: If Item<Data[mid],then:                                   |
-|            set End:=mid-1                                        |
-|        else:                                                     |
-|            set Beg:=mid+1                                        |
-|step-4: set Mid:=Int((Beg+End)/2)                                 |
-|step-5: If Data[mid]=Item, then:                                  |
-|          set LOC:=mid                                            |
-|        Else:                                                     |
-|            set LOC:=NULL                                         |
-|step-6: Exit                                                      |
-_-------------------------------------------------------------------
+|---------------------------------------|
+|Algorithm:                             |
+|step-1: If N=0, set fact:=1 and return |
+|step-2: call factorial(fact, N-1)      | 
+|step-3: set fact:=N*fact               | 
+|step-4: return                         | 
+|---------------------------------------|
 */
-
 #include <bits/stdc++.h>
-using namespace std; 
+using namespace std;
+
+int factorial(int n){
+    if(n == 0){
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
 
 int main(){
-    int n, search_item;
-    cout << "Enter the size of array: ";
+    int n;
+    cout << "Enter the positive number: ";
     cin >> n;
-    int arr[n+1];
-    cout << "Enter the sorted element of the array: ";
-    for(int i = 1; i <= n; i++){
-        cin >> arr[i];
-    }
-    cout << "Enter the search item: ";
-    cin >> search_item;
-    int begin = 1, endd = n + 1;
-    while (begin <= endd){
-        int mid = (begin + endd)/2;
-        if(arr[mid] == search_item){
-            cout << "Search element found in index " << mid << '\n';
-            return 0;
-        }
-        else if(arr[mid] < search_item){
-            begin = mid + 1;
-        }
-        else{
-            endd = mid - 1;
-        }
-    } 
-    cout << "Sorry!!.. The search item not found in the array..\n";
+    cout << "The Factorial of " << n << " is : ";
+    cout << factorial(n) << '\n';
     return 0;
 }
 
+
 /*
-----------------------------
-time complexity: O(log(n))
+-----------------------
+time complexity: O(n)
 space complexity: O(n)
----------------------------
+-----------------------
 */
