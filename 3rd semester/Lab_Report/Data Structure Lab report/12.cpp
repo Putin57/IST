ed2be50cedea85c0d873612fffb4054cd68d4c69
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-12-18 03:18:57 +0600

    Update 12.cpp

--- 3rd semester/Lab_Report/Data Structure Lab report/12.cpp
+++ 3rd semester/Lab_Report/Data Structure Lab report/12.cpp
@@ -1,1 +1,34 @@
-// Under construction.....
+// 12. Write a program to traverse a binary tree in any order.
+
+/*
+-------------------------------------------------------------------------------------------------
+Algorithm:  
+[N:B: A binary tree can be traversed in three primary orders: In-order, Pre-order, Post-order.
+but there is None of them mention in the question. 
+So, No Mention means, No Algorithm. now, keep smile and happy coding ^_^]
+------------------------------------------------------------------------------------------------
+*/
+
+// In-order(left, root, right)
+#include <bits/stdc++.h>
+using namespace std;
+struct Node {
+    int data;
+    Node *left,*right;
+    Node(int val):data(val),left(nullptr),right(nullptr){}
+};
+void traverse(Node* root) {
+    if(!root)return;
+    traverse(root->left);          
+    cout<<root->data<<' ';
+    traverse(root->right);         
+}
+int main(){
+    Node*root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    traverse(root); 
+    return 0;
+}
