0494c82c6963e3d3903a808b1cdda0f6a87dd712
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2024-12-19 04:53:41 +0600

    Update 4.cpp

--- 3rd semester/Lab_Report/OOP Lab report/4.cpp
+++ 3rd semester/Lab_Report/OOP Lab report/4.cpp
@@ -1,12 +1,26 @@
 /*
-4. Write a program to append the contents of a file into another.
+4.Write a program to for calculating of three given numbersin three different classes using concept of 
+function overloading.
 */
+
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
+class Area {
+    public:
+        void area(float a, float b, float c){cout << "The area of Triangle: " << 0.5 * a * b << '\n';}
+        void area(float b, float c){cout << "The area of Rectangle is: " << b * c << '\n';}
+        void area(float r){cout << "The area of Circle is: " << 3.1416 * r * r << '\n';}
+};
 int main() {
-    ifstream src("main.txt");
-    ofstream dest("append.txt",ios::app);
-    dest << '\n' << src.rdbuf();
+    Area a;
+    float height, length, width, radius;
+    cout << "\nEnter the height: "; cin >> height;
+    cout << "Enter the length: "; cin >> length;
+    cout << "Enter the width: "; cin >> width;
+    cout << "Enter the radius: "; cin >> radius;
+    a.area(height, length, width);
+    a.area(length, width);
+    a.area(radius);
     return 0;
 }
