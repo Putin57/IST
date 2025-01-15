b91a61d9f95d99099e61f7b13a5acfa7f8631b82
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2025-01-15 23:54:48 +0600

    Create 2_digit_copy_and_paste.asm

---
+++ 4th semester/Assembly Language/2_digit_copy_and_paste.asm
@@ -0,0 +1,28 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+
+MAIN PROC
+    MOV AH, 01  ; INPUT
+    INT 21H     ; VALUE STORED IN AL  
+    MOV BL, AL 
+    INT 21H
+    MOV BH, AL      
+    
+    MOV AH, 02 ; OUTPUT WITH NEWLLINE  
+    MOV DL,10
+    INT 21H  
+    MOV DL, 13
+    INT 21H 
+    
+    
+    
+    MOV DL, BL  ; DL = AL
+    INT 21H      
+    MOV DL,BH   ; DL = BH
+    INT 21H
+    MOV AH, 4CH
+    INT 21H
+    MAIN ENDP
+END MAIN
