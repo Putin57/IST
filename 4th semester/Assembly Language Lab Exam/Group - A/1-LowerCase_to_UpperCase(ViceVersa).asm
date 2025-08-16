4cfad6b2642d5a4366bd499b52ee30889c6db3c1
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2025-08-16 23:11:31 +0600

    Update 1-LowerCase_to_UpperCase(ViceVersa).asm

--- 4th semester/Assembly Language Lab Exam/Group - A/1-LowerCase_to_UpperCase(ViceVersa).asm
+++ 4th semester/Assembly Language Lab Exam/Group - A/1-LowerCase_to_UpperCase(ViceVersa).asm
@@ -1,17 +1,13 @@
 .model small
 .stack 100h
 .code
-main proc
+start:
     mov ah, 1        ; read char
     int 21h  
-    
     xor al, 20h      ; toggle case
-    mov dl, al   
-    
+    mov dl, al     
     mov ah, 2        ; print char
     int 21h       
-    
     mov ah, 4ch      ; exit
     int 21h
-    main endp
-end main
+end start
