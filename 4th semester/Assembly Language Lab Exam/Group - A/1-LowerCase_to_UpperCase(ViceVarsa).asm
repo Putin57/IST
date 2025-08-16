b429e18e064bee753f11086092a3a1142ca842ea
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2025-08-16 12:54:08 +0600

    Create 1-LowerCase_to_UpperCase(ViceVarsa).asm

---
+++ 4th semester/Assembly Language Lab Exam/Group - A/1-LowerCase_to_UpperCase(ViceVarsa).asm
@@ -0,0 +1,18 @@
+; Short 8086 program to toggle case of a letter
+.model small
+.stack 100h
+.code
+main proc
+    mov ah, 1        ; read char
+    int 21h  
+    
+    xor al, 20h      ; toggle case
+    mov dl, al   
+    
+    mov ah, 2        ; print char
+    int 21h       
+    
+    mov ah, 4Ch      ; exit
+    int 21h
+    main endp
+end main
