820bcb96be7b32df6957690eba1e5f52bfcb08eb
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2025-08-16 23:09:01 +0600

    Update 4-add_two_decimal_number.asm

--- 4th semester/Assembly Language Lab Exam/Group - A/4-add_two_decimal_number.asm
+++ 4th semester/Assembly Language Lab Exam/Group - A/4-add_two_decimal_number.asm
@@ -1,7 +1,7 @@
 .model small
 .stack 100h
 .code
-main proc
+start:
     mov ah,1
     int 21h
     sub al,'0'
@@ -22,5 +22,4 @@ main proc
 
     mov ah,4ch
     int 21h
-    main endp
-end main
+end start
