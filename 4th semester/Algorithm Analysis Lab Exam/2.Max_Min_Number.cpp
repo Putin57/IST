1238a920d7945862296446af456391bb8587f431
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2025-08-18 01:13:12 +0600

    Create 2.Max_Min_Number.cpp

---
+++ 4th semester/Algorithm Analysis Lab Exam/2.Max_Min_Number.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+vector<int>merge_sort(int n, vector<int>&arr){
+  if(n > 1){
+    int v1 = n/2;
+    int v2 = n - v1;
+    vector<int>left(v1), right(v2);
+    for(int i = 0; i < v1; i++){
+      left[i] = arr[i];
+    }
+    for(int i = v1; i < n; i++){
+      right[i - v1] = arr[i];
+    }
+    merge_sort(v1, left);
+    merge_sort(v2, right);
+    int i = 0, j = 0, k = 0;
+    while(i < v1 and j < v2){
+      if(left[i] < right[j]){
+        arr[k] = left[i];
+        i++, k++;
+      }
+      else{
+        arr[k] = right[j];
+        j++; k++;
+      }
+    }
+    while(i < v1){
+      arr[k] = left[i];
+      i++; k++;
+    }
+    while(j < v2){
+      arr[k] = right[j];
+      j++; k++;
+    }
+  }
+  return arr;
+}
+
+int main(){
+  cout << "Enter the size of array: ";
+  int n; cin >> n;
+  cout << "Enter the element of array: ";
+  vector<int>arr(n);
+  for(int i = 0; i < n; i++){
+    cin >> arr[i];
+  }
+  vector<int>arr2 = merge_sort(n,arr);
+  cout << "Minimum: "<< arr2.front() << "   Maximum:" << arr2.back() << '\n';
+  return 0;
+
+}
+
+
+/*
+time complexity: O(nlog n)
+space complexity: O(N)
+
+*/
